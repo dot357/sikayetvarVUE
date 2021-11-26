2fca25e4bc359f2e9ec2890b3fc4c5570c27f512
Emre Can Eskimez <emrecaneskimez@outlook.com>
2021-11-26 22:59:06 +0300

    modified:   src/App.vue 	new file:   src/assets/bg.png 	modified:   src/assets/logo.png 	modified:   src/router/index.js 	deleted:    src/views/About.vue 	src/components/Footer.vue 	src/components/Header.vue 	src/components/Modal.vue 	src/components/buttons/ 	src/views/DynamicPosts.vue 	src/views/DynamicProfile.vue 	src/views/Posts.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import(/* webpackChunkName: "about" */ '../views/Posts.vue')
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/posts',
+    name: 'Posts',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Posts.vue')
+  },
+  {
+    path: '/posts/:id',
+    
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DynamicPosts.vue')
+  },
+  {
+    path: '/profile/:id',
+    
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/DynamicProfile.vue')
   }
 ]
 
